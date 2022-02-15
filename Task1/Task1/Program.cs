ec4d8dfba9950ed2553dcd75c74d9a6f67b99415
Your Greyzlion <hamidsha@code.edu.az>
2022-02-15 06:36:07 +0400

    HW1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main() 
+        {
+            #region Task1
+            Console.WriteLine("Please enter three values");
+            Console.ReadLine();
+            int num1=0,num2=0,num3=0;
+            int largest = 0;
+            if (num2 > num1)
+              Console.WriteLine (largest = num2);
+            if (num3 > largest)
+             Console.WriteLine (largest = num3);
+
+            return ;
+            #endregion
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
